cc4b311cb11e4ca7a7b24da44a3cd0e2b9213628
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-19 11:32:00 -0700

    solving the core problem of sum

--- summ.js
+++ summ.js
@@ -3,3 +3,21 @@
 // Do support negative numbers though. If any argument is not a number, output an error message. We
 // need at least 2 arguments. 
 
+// get the arguments
+const args = process.argv.slice(2);
+console.log("args:", args);
+
+// create an accumulator for the sum
+let total = 0;
+
+// iterate through the command line args
+// for off
+
+for (let arg of args) {
+  total += Number(arg);
+
+  console.log("args:", typeof Number(arg), "total:", total);
+}
+
+
+console.log("Total:", total);
